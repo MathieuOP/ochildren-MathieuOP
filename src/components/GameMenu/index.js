279579f4c85e8f9ffc696fb8a5b3221ae6c76d56
AnatoleLucet <lucet.anatole@gmail.com>
2019-05-07 09:40:56 +0200

    final menu

--- src/components/GameMenu/index.js
+++ src/components/GameMenu/index.js
@@ -4,33 +4,31 @@ import { FaGamepad, FaPuzzlePiece, FaCheckSquare } from 'react-icons/fa';
 
 import './index.scss';
 
-const GameMenu = () => {
-  return (
-    <div className="gameMenu">
-      <nav className="gameMenu-nav">
-        <ul>
-          <li>
-            <Link to="home-game/jeux/discovery">
-              <FaGamepad size="3em" />
-              <span>Discovery</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/home-game/jeux/quiz">
-              <FaCheckSquare size="2em" className="gameMenu-icon--square" />
-              <span className="gameMenu-link--quiz">Quiz</span>
-            </Link>
-          </li>
-          <li>
-            <Link to="/home-game/jeux/puzzle">
-              <FaPuzzlePiece size="2em" className="gameMenu-icon--puzzle" />
-              <span className="gameMenu-link--puzzle">Puzzle</span>
-            </Link>
-          </li>
-        </ul>
-      </nav>
-    </div>
-  );
-};
+const GameMenu = () => (
+  <div className="gameMenu">
+    <nav className="gameMenu-nav">
+      <ul>
+        <li>
+          <Link to="home-game/jeux/discovery">
+            <FaGamepad size="3em" />
+            <span>Discovery</span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/home-game/jeux/quiz">
+            <FaCheckSquare size="2em" className="gameMenu-icon--square" />
+            <span className="gameMenu-link--quiz">Quiz</span>
+          </Link>
+        </li>
+        <li>
+          <Link to="/home-game/jeux/puzzle">
+            <FaPuzzlePiece size="2em" className="gameMenu-icon--puzzle" />
+            <span className="gameMenu-link--puzzle">Puzzle</span>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  </div>
+);
 
 export default GameMenu;
