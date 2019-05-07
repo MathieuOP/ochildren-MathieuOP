497669d0bdc9fdd12a0e9c543b17a954655984bc
AnatoleLucet <lucet.anatole@gmail.com>
2019-05-07 13:59:19 +0200

    c'est le monsieur qui m'a dit de commit

--- src/store/ajaxMiddleware.js
+++ src/store/ajaxMiddleware.js
@@ -1,29 +1,27 @@
 import axios from 'axios';
-import { 
-  DATA_HOME_PAGE
-} from './reducer';
+import { DATA_HOME_PAGE } from './reducer';
 // import {} from './reducer';
 
-const ajaxMiddleware = store => next => (action) => {
+const ajaxMiddleware = store => next => action => {
   switch (action.type) {
     case DATA_HOME_PAGE:
-
-      axios.get(`http://localhost/Ochildren-Back/public/api/worlds`, {
-      })
-        .then((response) => {
-          store.dispatch(next({
-            ...action,
-            data: response.data,
-          }));
+      axios
+        .get(`http://localhost/Ochildren-Back/public/api/worlds`, {})
+        .then(response => {
+          store.dispatch(
+            next({
+              ...action,
+              data: response.data
+            })
+          );
         })
-        .catch((error) => {
+        .catch(error => {
           // console.log(error);
-          
         });
       break;
     default:
       return next(action);
   }
-}
+};
 
 export default ajaxMiddleware;
