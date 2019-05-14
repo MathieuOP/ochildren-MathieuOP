770171b068de44b01cd1581f12c7e0b8eef5fb5e
AnatoleLucet <lucet.anatole@gmail.com>
2019-05-14 13:06:41 +0200

    final 404

---
+++ src/components/Page404/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './index.scss';
+
+const Page404 = () => {
+
+  return (
+    <div className="page404">
+      <h1>Page not found</h1>
+    </div>
+  )
+}
+
+export default Page404;
